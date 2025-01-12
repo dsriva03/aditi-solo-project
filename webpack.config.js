d3f86693ac5d73b0d0be38a3f26c24f1e61d0be6
dsriva03 <dsriva03@gmail.com>
2025-01-12 17:54:26 -0500

    FINISHED WEBPACK :DDDDD

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,69 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    mode: process.env.NODE_ENV, //environment- production or development code 
+    mode: process.env.NODE_ENV || 'development', //environment- production or development code, default to development code
 //tell webpack where my entry file is- the very first file to start the chain reaction
-    entry: './src/index.js',
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'build'), //output dirctory
+        filename: 'bundle.js', //a bundle of everything here
+        clean: true, //cleans the build directory before each build
     },
     module: {
         rules: [
             {
-                test: /\.jsx?/,
+                test: /\.jsx?$/, //regex that looks for files that ends with js or jsx
                 exclude: /node_modules/,
+                use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/env', '@babel/react'],
+                    presets: [
+                        '@babel/env', //transpile modern javascript
+                        '@babel/react', //transpile JSX
+                    ],
                 },
             },
+        },
             {
-                test: /\.s?css/,
-                use: ['style-loader', 'css-loader', 'sass-loader'] //! do we need to install this in the terminal??
+                test: /\.s?css$/, //matches css and scss files
+                use: ['style-loader', 'css-loader', 'sass-loader'], //! do we need to install this in the terminal??
             },
         ],
     },
     plugins: [
-        
-    ]
+        new HtmlWebpackPlugin({
+            //generates index.html file
+            title: 'Development',
+            template: path.resolve(__dirname, 'public/index.html'),
+        }),
+    ],
+    devServer: {
+        //allows for hot reloading
+        static: {
+            publicPath: '/',
+            directory: path.resolve(__dirname, 'public')
+        },
+        port: 8000, 
+        hot: true,
+        open: true,
+        proxy: [
+            {
+                context: ['/api'],
+                target: 'http://localhost:3000'
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
 
 
 //install babel loaders
 //figure out src and dist
 //create index.html 
 //add start and build scripts
-}
+};
 
